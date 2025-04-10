ca72143173a9c3f274c9708d17554c99b6d4fb94
Eitan Elberg <eitan.elberg@consumrz.com>
2025-04-10 18:38:11 +0300

    bento

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -91,13 +91,18 @@ export const BentoGridItem = ({
         )}
       </div>
 
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
-        {/* {icon} */}
-        <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
-          {title}
-        </div>
-        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
+      <div
+        className={cn(
+          titleClassName,
+          "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+        )}
+      >
+        <div className="font-sans text-sm font-extralight text-[#c1c2d3] md:text-xs lg:text-base z-10">
           {description}
+        </div>
+
+        <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+          {title}
         </div>
       </div>
     </div>
